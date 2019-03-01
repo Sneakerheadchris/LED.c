0103221123f726399968492814bbd0ec199d9e9e
Sneakerheadchris <47801724+Sneakerheadchris@users.noreply.github.com>
2019-03-01 13:27:35 -0500

    Update led.c

--- led.c
+++ led.c
@@ -7,36 +7,21 @@ void setup() {
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
 }
 void loop (){
-    leds[0]=0xffffff;FastLED.show();delay(5);
-    leds[0]=0x000000;FastLED.show();delay(5); 
-    leds[9]=0x4fa547;FastLED.show();delay(5);
-    leds[9]=0x000000;FastLED.show();delay(5);
-    leds[18]=0xe51b1b;FastLED.show();delay(5);
-    leds[18]=0x000000;FastLED.show();delay(5);
-    leds[27]=0x873721;FastLED.show();delay(5);
-    leds[27]=0x000000;FastLED.show();delay(5);
-    leds[36]=0xed6504;FastLED.show();delay(5);
-    leds[36]=0x000000;FastLED.show();delay(5);
-    leds[45]=0x5b5957;FastLED.show();delay(5); 
-    leds[45]=0x000000;FastLED.show();delay(5);
-    leds[54]=0xfc7bd3;FastLED.show();delay(5);
-    leds[54]=0x000000;FastLED.show();delay(5);
-    leds[63]=0xfff200;FastLED.show();delay(5);
-    leds[63]=0x000000;FastLED.show();delay(5);
-    leds[72]=0x4fa547;FastLED.show();delay(5);
-    leds[72]=0x000000;FastLED.show();delay(5); 
-    leds[81]=0xe51b1b;FastLED.show();delay(5);
-    leds[81]=0x000000;FastLED.show();delay(5);
-    leds[90]=0x873721;FastLED.show();delay(5);
-    leds[90]=0x000000;FastLED.show();delay(5);
-    leds[99]=0xed6504;FastLED.show();delay(5);
-    leds[99]=0x000000;FastLED.show();delay(5);
-    leds[108]=0x5b5957;FastLED.show();delay(5);
-    leds[108]=0x000000;FastLED.show();delay(5);
-    leds[117]=0xfc7bd3;FastLED.show();delay(5); 
-    leds[117]=0x000000;FastLED.show();delay(5);
-    leds[126]=0xfff200;FastLED.show();delay(5);
-    leds[126]=0x000000;FastLED.show();delay(5);
-    leds[135]=0xffffff;FastLED.show();delay(5);
-    leds[135]=0x000000;FastLED.show();delay(5);
+    leds[0,72]=0xffffff;FastLED.show();delay(5);
+    leds[0,72]=0x000000;FastLED.show();delay(5); 
+    leds[9,81]=0x4fa547;FastLED.show();delay(5);
+    leds[9,81]=0x000000;FastLED.show();delay(5);
+    leds[18,90]=0xe51b1b;FastLED.show();delay(5);
+    leds[18.90]=0x000000;FastLED.show();delay(5);
+    leds[27,99]=0x873721;FastLED.show();delay(5);
+    leds[27,99]=0x000000;FastLED.show();delay(5);
+    leds[36,108]=0xed6504;FastLED.show();delay(5);
+    leds[36,108]=0x000000;FastLED.show();delay(5);
+    leds[45,117]=0x5b5957;FastLED.show();delay(5); 
+    leds[45,117]=0x000000;FastLED.show();delay(5);
+    leds[54,126]=0xfc7bd3;FastLED.show();delay(5);
+    leds[54,126]=0x000000;FastLED.show();delay(5);
+    leds[63,135]=0xfff200;FastLED.show();delay(5);
+    leds[63,135]=0x000000;FastLED.show();delay(5);
 }
+    
